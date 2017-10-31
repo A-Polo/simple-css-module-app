4716a8daf58ee4aab1923ccb2b50850522a9849e
Anton Polomkin <aap@0814c0003.local>
2017-10-31 15:22:08 +0200

    Add simple app

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+	entry:  './src',
+	output: {
+		path: __dirname + '/build',
+		filename: 'bundle.js',
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js/,
+				loader: 'babel-loader',
+				include: __dirname + '/src',
+			},
+			{
+				test: /\.css/,
+				loader: ExtractTextPlugin.extract('css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]')
+			}
+		]
+	},
+	plugins: [
+		new ExtractTextPlugin("styles.css")
+	]
+};
